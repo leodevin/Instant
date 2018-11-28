286eb7286e8a2f1756fc469cfb48550b1cde5ad5
leodevin <leonard.devincre@free.fr>
2018-11-28 16:47:10 +0100

    Initial Commit

---
+++ Instant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instant
+//
+//  Created by Léonard Devincre on 28/11/2018.
+//  Copyright © 2018 Léonard Devincre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
